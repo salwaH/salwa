cee22ed9977cccd28fb44a4e3cd7bd8abb99a1e3
salwaH <smh32@mail.aub.edu>
2014-10-25 18:36:54 +0200

    Assignment 2 Salwa Haidar
    Assignment 2

---
+++ cachematrix.R
@@ -0,0 +1,29 @@
+## Write functions that will cache the inverse of a matrix
+
+## This function creates a special matrix object that can cache its inverse
+makeCacheMatrix <- function(x = matrix()) {
+      i<-NULL
+      set<-function(y){
+            x<<-y
+            i<<-NULL
+      }
+      get<-function()x
+      setinv<-function(solve)i<<-solve
+      getinv<-function()i
+      list(set=set,get=get,setinv=setinv,getinv=getinv)
+}
+## this function computes the inverse of the spectial matrix returned by makeCacheMatirx
+## if the inverse has already been calculated , then cachesolve should retrieve the inverse 
+##from the cache
+cacheSolve <- function(x, ...) {
+      ## Return a matrix that is the inverse of 'x'
+      i<-x$getinv()
+      if(!is.null(i)){
+            message("getting cached data")
+            return (i)
+      }
+      data<-x$get()
+      i<-solve(data,...)
+      x$setinv(i)
+      i
+}
